9d76e3a2527d6bbca1c3876a5bd60800f33a53f9
nicomiguelino <nicomiguelino2014@gmail.com>
2020-06-09 21:35:01 +0800

    Added session manipulation - add, modify, and access - in views.

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from main.views import IndexView
+from main.views import (
+    IndexView, SessionReadWriteView, ClearSessionView)
 
 app_name = 'main'
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('session_read_write/', SessionReadWriteView.as_view(), name='session_read_write'),
+    path('clear_session/', ClearSessionView.as_view(), name='clear_session')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,30 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.views import View
 
 class IndexView(View):
     def get(self, request):
+        if request.session.test_cookie_worked():
+            request.session.delete_test_cookie()
+        else:
+            request.session.set_test_cookie()
+            messages.error(request, 'Please enable cookie')
+
         return render(request, 'main/index.html')
+
+class SessionReadWriteView(View):
+    def get(self, request):
+        request.session['user_id'] = 20
+        request.session['team'] = 'Barcelona'
+
+        return render(request, 'main/session_read_write_view.html')
+
+class ClearSessionView(View):
+    def get(self, request):
+        try:
+            del request.session['user_id']
+            del request.session['team']
+        except KeyError:
+            pass
+
+        return render(request, 'main/clear_session.html')
