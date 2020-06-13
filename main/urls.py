3751346158ba5a03997d62e5af5262813f48caed
nicomiguelino <nicomiguelino2014@gmail.com>
2020-06-13 15:28:49 +0800

    Added views for built-in test cookies

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+
 from main.views import (
-    IndexView, SessionReadWriteView, ClearSessionView)
+    IndexView, SessionReadWriteView, ClearSessionView, TestCookie01,
+    TestCookie02
+)
+
 
 app_name = 'main'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('session_read_write/', SessionReadWriteView.as_view(), name='session_read_write'),
-    path('clear_session/', ClearSessionView.as_view(), name='clear_session')
+    path('clear_session/', ClearSessionView.as_view(), name='clear_session'),
+
+    path('test_cookie_01/', TestCookie01.as_view(), name='test_cookie_01'),
+    path('test_cookie_02/', TestCookie02.as_view(), name='test_cookie_02')
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.shortcuts import render
 from django.views import View
 
+
 class IndexView(View):
     def get(self, request):
         if request.session.test_cookie_worked():
@@ -12,12 +13,14 @@ class IndexView(View):
 
         return render(request, 'main/index.html')
 
+
 class SessionReadWriteView(View):
     def get(self, request):
         request.session['user_id'] = 20
         request.session['team'] = 'Barcelona'
 
         return render(request, 'main/session_read_write_view.html')
+
 
 class ClearSessionView(View):
     def get(self, request):
@@ -28,3 +31,19 @@ class ClearSessionView(View):
             pass
 
         return render(request, 'main/clear_session.html')
+
+
+class TestCookie01(View):
+    def get(self, request):
+        request.session.set_test_cookie()
+        return render(request, 'main/test_cookie_01.html')
+
+
+class TestCookie02(View):
+    def get(self, request):
+        context = {}
+        if (request.session.test_cookie_worked()):
+            context.update({'cookie_status': 'Test cookie worked!'})
+            request.session.delete_test_cookie()
+
+        return render(request, 'main/test_cookie_02.html', context)
