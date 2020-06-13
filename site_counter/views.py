1feacb4e6f8124d4a4a058a559e36a5ac3fa2068
nicomiguelino <nicomiguelino2014@gmail.com>
2020-06-13 18:13:26 +0800

    Modified site_counter implementation to use sessions instead of cookies

--- site_counter/views.py
+++ site_counter/views.py
@@ -8,25 +8,26 @@ class IndexView(View):
     def get(self, request):
         context_dict = {}
 
-        visits = int(request.COOKIES.get('visits', '1'))
+        visits = request.session.get('visits', 1)
         reset_last_visit_time = False
 
-        if 'last_visit' in request.COOKIES:
-            last_visit = request.COOKIES['last_visit']
+        last_visit = request.session.get('last_visit')
+        if last_visit:
             last_visit_time = datetime.strptime(
                 last_visit[:-7], "%Y-%m-%d %H:%M:%S")
 
-            if (datetime.now() - last_visit_time).days > 0:
+            if (datetime.now() - last_visit_time).seconds > 0:
                 visits = visits + 1
                 reset_last_visit_time = True
         else:
             reset_last_visit_time = True
-            context_dict['visits'] = visits
-
-        response = render(request, 'site_counter/index.html', context_dict)
 
         if reset_last_visit_time:
-            response.set_cookie('last_visit', datetime.now())
-            response.set_cookie('visits', visits)
+            request.session['last_visit'] = str(datetime.now())
+            request.session['visits'] = visits
+
+        context_dict['visits'] = visits
+
+        response = render(request, 'site_counter/index.html', context_dict)
 
         return response
